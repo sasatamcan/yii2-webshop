4aad1025c1ce3e0cf0bd960cffc7a0082abb863f
sasatamcan <tamcanbaris@gmail.com>
2019-07-23 01:10:50 +0300

    cart

--- controllers/CartController.php
+++ controllers/CartController.php
@@ -40,5 +40,30 @@ class CartController extends Controller{
         $this->layout = false;
         return $this->render('cart-modal', compact('session'));
     }
+    public function actionClear(){
+        $session =Yii::$app->session;
+        $session->open();
+        $session->remove('cart');
+        $session->remove('cart.qty');
+        $session->remove('cart.sum');
+        $this->layout = false;
+        return $this->render('cart-modal', compact('session'));
+    }
+    public function actionDelItem(){
+        $id = Yii::$app->request->get('id');
+        $session =Yii::$app->session;
+        $session->open();
+        $cart = new Cart();
+        $cart->recalc($id);
+        $this->layout = false;
+        return $this->render('cart-modal', compact('session'));
+    }
+
+    public function actionShow(){
+        $session =Yii::$app->session;
+        $session->open();
+        $this->layout = false;
+        return $this->render('cart-modal', compact('session'));
+    }
 
 }
--- views/cart/cart-modal.php
+++ views/cart/cart-modal.php
@@ -2,32 +2,32 @@
     <div class="table-responsive">
         <table class="table table-hover table-striped">
             <thead>
-                <tr>
-                    <th>Фото</th>
-                    <th>Наименование</th>
-                    <th>Кол-во</th>
-                    <th>Цена</th>
-                    <th><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></th>
-                </tr>
+            <tr>
+                <th>Фото</th>
+                <th>Наименование</th>
+                <th>Кол-во</th>
+                <th>Цена</th>
+                <th><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></th>
+            </tr>
             </thead>
             <tbody>
             <?php foreach($session['cart'] as $id => $item):?>
                 <tr>
-                    <td><?= $item['image']?></td>
+                    <td><?= \yii\helpers\Html::img("web/uploads/{$item['image']}", ['alt' => $item['name'], 'height' => 50]) ?></td>
                     <td><?= $item['name']?></td>
                     <td><?= $item['qty']?></td>
                     <td><?= $item['price']?></td>
-                    <td><span class="glyphicon glyphicon-remove text-danger del-item" aria-hidden="true"></span></td>
+                    <td><span data-id="<?= $id?>" class="glyphicon glyphicon-remove text-danger del-item" aria-hidden="true"></span></td>
                 </tr>
             <?php endforeach?>
-                <tr>
-                    <td colspan="4">Итого: </td>
-                    <td><?= $session['cart.qty']?></td>
-                </tr>
-                <tr>
-                    <td colspan="4">На сумму: </td>
-                    <td><?= $session['cart.sum']?></td>
-                </tr>
+            <tr>
+                <td colspan="4">Итого: </td>
+                <td><?= $session['cart.qty']?></td>
+            </tr>
+            <tr>
+                <td colspan="4">На сумму: </td>
+                <td><?= $session['cart.sum']?></td>
+            </tr>
             </tbody>
         </table>
     </div>
